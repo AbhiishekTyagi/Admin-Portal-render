79f86aefbe7a0536ec7571bc4c7be1b7747a3d38
AbhiishekTyagi <142976495+AbhiishekTyagi@users.noreply.github.com>
2026-01-11 15:46:49 +0530

    Update index.html

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const URI="mongodb://127.0.0.1:27017/mern_admin";
+
 const URI=process.env.MONGODB_URI;
 
 const connectDB= async()=>{
